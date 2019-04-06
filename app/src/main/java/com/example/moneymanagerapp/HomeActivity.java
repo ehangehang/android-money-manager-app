1b232ee3b2eeedf8bb72e02e9c198c0bc721b5ff
ehangehang <rayhansuryatama@gmail.com>
2019-04-06 23:37:34 +0700

    fixing some UI and adding RV adapter

--- app/src/main/java/com/example/moneymanagerapp/HomeActivity.java
+++ app/src/main/java/com/example/moneymanagerapp/HomeActivity.java
@@ -1,16 +1,27 @@
 package com.example.moneymanagerapp;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private Button buttonTry;
+    private ImageButton openNav;
+    private TextView income, expense, aboutUs;
+
+    private DrawerLayout drawer;
+    private NavigationView navDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +31,66 @@ public class HomeActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_home);
 
-//        buttonTry = findViewById(R.id.button01);
-//        buttonTry.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(HomeActivity.this, IncomeActivity.class));
-//            }
-//        });
+        income = findViewById(R.id.incomeMenuDrawer);
+        expense = findViewById(R.id.expenseMenuDrawer);
+        aboutUs = findViewById(R.id.aboutUsMenuDrawer);
+        openNav = findViewById(R.id.garisTiga);
+        drawer = findViewById(R.id.drawer);
+        navDrawer = findViewById(R.id.nvView);
+        openNav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawer.openDrawer(Gravity.START);
+            }
+        });
+        setDrawerContent(navDrawer);
+
+        income.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(HomeActivity.this, "Income Menu", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        expense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(HomeActivity.this, "Expense Menu", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        aboutUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(HomeActivity.this, "About Us Menu", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void setDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    private void selectDrawerItem(MenuItem menuItem) {
+        Class fragmentClass;
+        switch (menuItem.getItemId()){
+            case R.id.incomeDrawer:
+            break;
+            case R.id.expenseDrawer:
+                break;
+            case R.id.aboutUs:
+                break;
+            default:
+        }
+
+        drawer.closeDrawers();
     }
 }
